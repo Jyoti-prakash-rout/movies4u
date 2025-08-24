92f2fa87446dfe61f687fb102c694245afc7a67d
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2025-08-24 15:48:05 +0530

    Started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="app">
+        <nav className="navbar"></nav>
+        <main></main>
+      </div>
+    </>
+  );
+}
+
+export default App;
